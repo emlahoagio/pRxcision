4774fee6c2e034465ac18e1cca2a2fa1d95fbe78
quantmdo <emlahoagio0610@gmail.com>
2022-09-05 16:08:12 +0700

    Add login and import form UI

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Authentication}/{action=Index}/{id?}");
 
 app.Run();
 
